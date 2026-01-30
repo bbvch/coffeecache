14e67f24779d90126af41ceda73643ddbc2872ed
Shpend Kelmendi <shekel7@hotmail.com>
2026-01-30 23:38:01 +0100

    fix links

--- src/components/AuthorsList/index.tsx
+++ src/components/AuthorsList/index.tsx
@@ -5,6 +5,13 @@ import Author from '@theme/Blog/Components/Author';
 export default function AuthorsList(): React.JSX.Element {
   Object.keys(authorsData).map((key) => {
     authorsData[key].url = `/authors/${key}`;
+
+    if (authorsData[key].socials?.linkedin) {
+      authorsData[key].socials.linkedin = "https://www.linkedin.com/in/" + authorsData[key].socials?.linkedin;
+    }
+    if (authorsData[key].socials?.github) {
+      authorsData[key].socials.github = "https://github.com/" + authorsData[key].socials?.github;
+    }
   });
 
   return (
