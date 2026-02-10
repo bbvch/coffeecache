3e79410ebe239cfd9af163fcd262e68fd7ad261c
Shpend Kelmendi <shekel7@hotmail.com>
2026-02-10 12:36:23 +0100

    use now correct base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://techinsights.bbv.ch',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/techinsights/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
